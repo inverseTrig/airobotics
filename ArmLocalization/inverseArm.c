f5a6cfeb228735ae0f736119b920b0c8c5d47e10
HeeChan Kang <heech.kang@gmail.com>
2018-02-12 16:10:37 -0600

    Adding and committing all changes made within the past few weeks.

---
+++ ArmLocalization/inverseArm.c
@@ -0,0 +1,26 @@
+/***************************************************
+ * HeeChan Kang
+ * CSC 431 - AI Robotics
+ * Assignment Zero - Arm Localization
+ ***************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+void main() {
+    double x, y, alpha;
+    double base = 1.0, l1 = 0.8, l2 = 0.8;
+
+    /* Take user input for alpha and beta in radians. */
+    printf("Enter x coordinate: ");
+    scanf("%lf", &x);
+    printf("Enter y coordinate: ");
+    scanf("%lf", &y);
+
+    /* Inverse Kinematics equation derived in class */
+    alpha = M_PI - acos((pow(x,2) + pow(y,2) - pow(l1,2) - pow(base,2))/(-2*l1*base));
+
+    printf("Current angle alpha is: %6.4lf.\n", alpha);
+
+}
